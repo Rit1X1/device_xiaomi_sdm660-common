0b94b46a0fcb4376e0e45ac6b78457777bec57ac
Rit1X1 <prdrit777@gmail.com>
2021-04-04 00:31:08 +0530

    Updating vendorsetup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,5 +7,9 @@ rm -rf vendor/qcom/opensource/data-ipa-cfg-mgr
 
 # clone
 git clone https://bitbucket.org/syberia-project/external_motorola_faceunlock.git -b 11.0 external/motorola/faceunlock
-git clone https://github.com/Rit1X1/android_vendor_codeaurora_telephony.git
-git clone https://github.com/ChrisW444/vendor_xiaomi_dirac vendor/xiaomi/dirac
+git clone https://github.com/Rit1X1/android_vendor_codeaurora_telephony.git vendor/codeaurora/telephony
+git clone https://github.com/ChrisW444/vendor_xiaomi_dirac vendor/dirac
+git clone https://github.com/xiaomi-sdm660/android_vendor_xiaomi_MiuiCamera.git vendor/xiaomi/MiuiCamera
+git clone https://github.com/OdSazib/vendor_xiaomi_wayne-common.git -b 11 vendor/xiaomi/wayne-common
+git clone https://github.com/OdSazib/vendor_xiaomi_sdm660-common.git -b 11 vendor/xiaomi/sdm660-common
+git clone https://github.com/Rit1X1/recovery_twrp.git recovery/twrp/xiaomi/jasmine_sprout
